275a6fda6907cb37168964ff68a254dcb7fbfe26
江 龙 <xiuhu@superbytech.com>
2026-05-14 11:50:28 +0800

    feat: 任务计数器

---
+++ task-counter-queue/index.ts
@@ -0,0 +1,36 @@
+type Listener = (visible: boolean) => void;
+
+export class TaskCounterQueue {
+    private queue: null[] = [];
+    private listeners = new Set<Listener>();
+
+    subscribe(listener: Listener) {
+        this.listeners.add(listener);
+        listener(this.queue.length > 0);
+        return () => this.listeners.delete(listener);
+    }
+
+    private notify() {
+        this.listeners.forEach((listener) => listener(this.queue.length > 0));
+    }
+
+    // 入队：只负责 push，闭眼添加
+    push() {
+        this.queue.push(null);
+        this.notify();
+    }
+
+    // 出队：只负责 shift，严格遵循先进先出
+    shift() {
+        if (this.queue.length > 0) {
+            this.queue.shift();
+            this.notify();
+        }
+    }
+
+    clear() {
+        this.queue = [];
+        this.notify();
+    }
+}
+
